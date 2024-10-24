6353fc31b6714b42b192c32509c3555d5808b7d1
Karani Wachira <wachirakeith@gmail.com>
2024-10-24 11:15:23 +0300

    feat: worked on simple age checker question 5

--- question5.py
+++ question5.py
@@ -1,12 +1,24 @@
 # Simple Age Checker
 
-
 # Write a Python program that asks the user for their age and tells them whether they are a minor
 # (under 18), an adult (18-65), or a senior (over 65)
 
 
+def age_checker():
+    #request user for age input
+    age = input("Enter your age: ")
+    age = int(age)
 
+    # logical condition to check whether is a minor and adult and senior
+    if age < 18:
+        print("You are a minor.")
+    elif age <= 65:
+        print("You are an adult.")
+    else:
+        print("You are a senior.")
 
+# function call
+age_checker()
 
 
 
--- question6.py
+++ question6.py
@@ -1,5 +1,4 @@
 # Password Validator
 
-
 # Write a Python program that asks the user to enter a password. If the password is "python123",
 # print "Access granted." Otherwise, print "Access denied."
