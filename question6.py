72d416b0ea630f456261d30f8624e1255948145a
Karani Wachira <wachirakeith@gmail.com>
2024-10-24 11:23:03 +0300

    feat: added solution to question 6

--- question6.py
+++ question6.py
@@ -2,3 +2,34 @@
 
 # Write a Python program that asks the user to enter a password. If the password is "python123",
 # print "Access granted." Otherwise, print "Access denied."
+
+def password_validator ():
+    password = input("Enter a password: ")
+
+    # validate the password
+    if password == "password123":
+        print("Access granted")
+
+    else: print("Access denied")
+
+# function call
+password_validator()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
